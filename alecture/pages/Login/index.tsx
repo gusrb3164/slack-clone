5becc26cb4a1ef081eaaa00a2ea84fd3f2d1582f
hyeon-9yu <kim.h.1@chunmyung.com>
2021-07-24 23:38:45 +0900

    feat: 회원가입, 로그인 페이지 추가

---
+++ alecture/pages/Login/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import useInput from '@hooks/useinput';
+import axios from 'axios';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+
+const Login = () => {
+  const { data, error, revalidate } = useSWR('/api/users', fetcher);
+  const [loginError, setLoginError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios
+        .post('/api/users/login', { email, password }, { withCredentials: true })
+        .then(() => {
+          revalidate();
+        })
+        .catch((error) => {
+          setLoginError(error.response?.data?.statusCode === 401);
+        });
+    },
+    [email, password],
+  );
+
+  if (data) {
+    return <Redirect to="/workspace/channel" />;
+  }
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+          {loginError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default Login;
---
+++ alecture/pages/SignUp/index.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import useInput from '@hooks/useinput';
+import axios from 'axios';
+import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+
+const SignUp = () => {
+  const { data, error, revalidate } = useSWR('/api/users', fetcher);
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, , setPassword] = useInput('');
+  const [passwordCheck, , setPasswordCheck] = useInput('');
+  const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (mismatchError || !nickname) {
+        console.log('에러');
+        return;
+      }
+      axios
+        .post('/api/users', { email, nickname, password })
+        .then((res) => setSignUpSuccess(true))
+        .catch((err) => {
+          console.log(err.response);
+          setSignUpError(err.response.data);
+        });
+    },
+    [email, nickname, password, passwordCheck],
+  );
+
+  if (data) {
+    return <Redirect to="/workspace/channel" />;
+  }
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <Link to="/login">로그인 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp;
